6616638b002991e605745ed7d3b2d988643f4b39
payomdousti <payomdousti@gmail.com>
2013-06-15 19:55:48 -0400

    initial shit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hotjams::Application.config.secret_token = '9ba67acd777eda7dd7a2ed1d5adcdf29c026207918fd7d07ba1290380720d314f5748c3db306400eae9e14136021fbe86e7c4a8e7ea66f797765ff4286f31285'
