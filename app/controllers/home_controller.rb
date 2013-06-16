9b8ae1e8bbd0078248fccdfc547d97cf09be4078
payomdousti <payomdousti@gmail.com>
2013-06-16 05:49:50 -0400

    I can embed the actual media in the site... getting closer. Need to implement a queue. Just make an ajax call that updates the src param

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-  	@graph = Koala::Facebook::API.new('CAACEdEose0cBAK0I0zofqf1Iwj3ZAQ4Hksrc7tFXYf39fNwtpnfWRQmliPOXZBY15Pz9poQZCmJzlCKzjmo0OJOkf3St3jySPJncSUWcdKt2RbyFoEoEz6pjkUbLGPDFerj1i249pUlcFkJTx6degNFlTd3qNXDvVaVNVH55gZDZD')
-  	@feed = @graph.get_connections("232990736786590", "feed", { :limit => 50} )
+  	@graph = Koala::Facebook::API.new('CAACEdEose0cBAFUTaOwthTavprQbPmMiGlPgj7ZBrCmKAhmLJ1GJHrqFufLs2wOZAcwXWD3zvq3z6ozKKQbMjOuUcYQZAH39Ag30HEZAYvqhfSKmEDXC4g0Qjxt8Ofjiqbmq8jhBts4LZCv6XIZBFfi3wdeDJr8b8ZD')
+  	@feed = @graph.get_connections("232990736786590", "feed", { :limit => 1} )
   end
 end
