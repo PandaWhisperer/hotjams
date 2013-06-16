60f5941380156a04f1b34db62d311dc26ec5137a
payomdousti <payomdousti@gmail.com>
2013-06-16 00:12:54 -0400

    i can display the group feed in raw data

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
-  	@oauth = Koala::Facebook::OAuth.new('136929479838758', 'e6d7efb8098c019e4647c9e2563ce753')
+  	@graph = Koala::Facebook::API.new('CAACEdEose0cBAK0I0zofqf1Iwj3ZAQ4Hksrc7tFXYf39fNwtpnfWRQmliPOXZBY15Pz9poQZCmJzlCKzjmo0OJOkf3St3jySPJncSUWcdKt2RbyFoEoEz6pjkUbLGPDFerj1i249pUlcFkJTx6degNFlTd3qNXDvVaVNVH55gZDZD')
+  	# @profile = @graph.get_object("232990736786590")
+  	@friends = @graph.get_connections("232990736786590", "feed")
   end
 end
