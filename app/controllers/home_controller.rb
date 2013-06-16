97121a2d6a2e529faab83d10712109e710155908
payomdousti <payomdousti@gmail.com>
2013-06-16 17:35:21 -0400

    holy shit I can display the feed and play songs with a single hidden player. proof of concept worked.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
-  	@graph = Koala::Facebook::API.new('CAACEdEose0cBAHPFBOL7nP66ymbuEyxhrmGAzJZCV7aNBdcbMZALD9rog7Q3Fc1WrCM7pp8TBL3cfVGkw4wA7oOiUGZB9ydxzdWqiljNtFXBGTKD77Esk3QSlvhxajZBxXVQL4ZCZBkT4sDzZCT1pBKO1D8K3dnaaPzbXs0aG8QXwZDZD')
-  	fb_feed = @graph.get_connections("232990736786590", "feed", { :limit => 1} )
+  	@graph = Koala::Facebook::API.new('CAACEdEose0cBAOrHSFPEqHSZA0i3YozZCYnp6Scyq31GEZAvdInpemM8OZALyABQ9utyYsXV3CEz9kGaze95xd6UOdwA4LjsQ7xQZAH4GMM623Wryf7lWeB6LKvf9V0rtVf4AuNGW2h1dyvY2ZBDWZA9GVb6vI1Mq9YtrxSCqAEAwZDZD')
+  	fb_feed = @graph.get_connections("232990736786590", "feed", { :limit => 20} )
   	@feed = []
   	fb_feed.each do |post|
   		@feed << post unless ( post.nil? || post['source'].nil? )
