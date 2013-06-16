a6928c64389c33c882e4b6de33d7d7a0a1c89fae
payomdousti <payomdousti@gmail.com>
2013-06-16 00:36:36 -0400

    list most recent 50 posters in hot jams

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
   def index
   	@graph = Koala::Facebook::API.new('CAACEdEose0cBAK0I0zofqf1Iwj3ZAQ4Hksrc7tFXYf39fNwtpnfWRQmliPOXZBY15Pz9poQZCmJzlCKzjmo0OJOkf3St3jySPJncSUWcdKt2RbyFoEoEz6pjkUbLGPDFerj1i249pUlcFkJTx6degNFlTd3qNXDvVaVNVH55gZDZD')
-  	# @profile = @graph.get_object("232990736786590")
-  	@friends = @graph.get_connections("232990736786590", "feed")
+  	@feed = @graph.get_connections("232990736786590", "feed", { :limit => 50} )
   end
 end
