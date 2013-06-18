1976b400bc3d13e63573d5fc10133c92f06e3124
payomdousti <payomdousti@gmail.com>
2013-06-17 22:45:59 -0400

    derp

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-  	@graph = Koala::Facebook::API.new('CAACEdEose0cBAFYBhi2A6rvyY30MHdNnE9h7GoiO7mvOgqmMSEteZAAehlEagDSBnDQLyKZBWV09GPxfyZB0r0xY5o3GsY66EZB7OEkYKZAHBDZBDwS6bVcuoC9vbTRZBBN31Sq775h5kjQj5gMDuKqrPyyBESActg2nYmKktTnugZDZD')
+  	@graph = Koala::Facebook::API.new('CAACEdEose0cBAIuk8N31grstt9C8lo0rwSZBVCPcqZBQq33JAA56pB93nVc5SP6CXwkX1IYwhMXZAu0j7eDwTGu6EZBB6nQZCwWLtk9uhzI7SudKMRnmcSILZAzcwUjFdjWZBKZBZC1dTNdS6MDgt3WDxg3pQoxZA6SjZAQhox8C4l4QwZDZD')
   	fb_feed = @graph.get_connections("232990736786590", "feed", { :limit => 20} )
   	@feed = []
   	fb_feed.each do |post|
