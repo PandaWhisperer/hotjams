e8193423311036cf2368b849951eb6b0afc910b2
Payom Dousti <payom@numberfire.com>
2013-08-13 02:42:29 -0400

    new 2 panel layout

--- app/models/jam.rb
+++ app/models/jam.rb
@@ -5,7 +5,7 @@ class Jam < ActiveRecord::Base
 
 
   def self.fetchAllFromFB
-	graph = Koala::Facebook::API.new('CAACEdEose0cBAA08cB6YZBap9xkdmDiInMFoaVuUdOb2c0SyiBSZAUOfYN5oNBIBJZC54q0ZBiFZChGifGqK2cw8fEjQDF4GPLUKV3GeXwt3bHkuhXqO2jtmJGwkF1vcf7jspa2wf3tUZA8Ti02EYqU5F4vdc9pKCrIji9hUl7LAZDZD')
+	graph = Koala::Facebook::API.new('CAACEdEose0cBABo1PrtCVVoUEzWvkgbfOuyyTZAg59BxfvHx3WsuijIEuxFwCrJc8BMZATnGbBffdtpeNbVcxPIZAfIhtpPHOqYdR0vzgUMZAWpj6GHMlipWrFcqd3rSCR93b5DDTaDd7BZC2Q5OiSYFPnNHQWugs5VFKZA2ifXQZDZD')
     fb_feed = graph.get_connections("232990736786590", "feed", { :limit => 20} )
     jams = []
     fb_feed.each do |post|
