9be4aece8a9a3c54731d3b3abf2c42096ccbe7c5
Payom Dousti <payom@numberfire.com>
2013-12-17 17:28:32 -0500

    fetch 200

--- app/models/jam.rb
+++ app/models/jam.rb
@@ -6,7 +6,7 @@ class Jam < ActiveRecord::Base
 
   def self.fetchAllFromFB
     graph = Koala::Facebook::API.new('CAACEdEose0cBABG5TkvYkmdXLJbZBNb4M3xTukxB5uzZCJKEqXnZCojyZCYEAjtTKOodLiZBpFVtpiV59i7sRFuMxgM0neZBpfuE5t83dWn50y46MsKWZByZAtumAyj9ZCBUoGHHzvRY37SK6ckBaMJsRmA5su6hQirUiOjzOn9nMHdLzBRNapZBkON5EWYFSIGWkZD')
-    fb_feed = graph.get_connections("232990736786590", "feed", { :limit => 2000} )
+    fb_feed = graph.get_connections("232990736786590", "feed", { :limit => 200} )
     jams = []
     fb_feed.each do |post|
       jams << post unless ( post.nil? || post['source'].nil? )
