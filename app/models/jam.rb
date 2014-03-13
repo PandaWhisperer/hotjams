803997cb43a16f2905f54c6d1d75ff1bd3c529ca
Payom Dousti <payom@numberfire.com>
2014-03-13 01:51:18 -0400

    this was so fucking buggy before... now it actually works as intended basically

--- app/models/jam.rb
+++ app/models/jam.rb
@@ -1,23 +1,23 @@
 class Jam < ActiveRecord::Base
-  attr_accessible :name, :source, :picture, :user
+  attr_accessible :name, :source, :picture, :user, :created_at, :updated_at
   belongs_to :user
   validates_uniqueness_of :source, :on => :create, :message => "must be unique"
 
 
   def self.fetchAllFromFB
     hotjams_page_id = "232990736786590";
-    user = User.order("updated_at").last
+    user = User.where("auth_token IS NOT NULL").order("updated_at").last
     access_token = user.auth_token
 
     graph = Koala::Facebook::API.new(access_token)
     
-    fb_feed = graph.get_connections(hotjams_page_id, "feed", { :limit => 200} )
-    
+    fb_feed = graph.get_connections(hotjams_page_id, "feed", { :limit => 500} )
     jams = [] 
     fb_feed.each do |post|
-      jams << post unless ( post.nil? || post['source'].nil? )
+      jams << post unless ( post.nil? || !post.has_key?('source') || post['source'].nil? )
     end
-    
+    jams
+
   end
 
 end
