9eed959bc7157db0cb97128918dd667c2e12a0ac
Payom Dousti <payom@numberfire.com>
2014-03-13 01:51:42 -0400

    use fb_id as the primary index basically

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :auth_token, :fb_id
   has_many :jams
-  validates_uniqueness_of :name, :on => :create, :message => "must be unique"
+  validates_uniqueness_of :fb_id, :on => :create, :message => "must be unique"
 end
